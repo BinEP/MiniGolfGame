3f36d623f3db8fa5bd98f47afc20d3b31754aa71
BinEP <bradystoffel@gmail.com>
2015-02-25 19:21:17 -0600

    Map class

---
+++ src/templates/Map.java
@@ -0,0 +1,18 @@
+package templates;
+
+public abstract class Map {
+
+	public static void main(String[] args) {
+		Map runIt = new Map();
+		runIt.runFromMain();
+	}
+
+	public void runFromMain() {
+
+	}
+
+	public Map() {
+		// TODO Auto-generated constructor stub
+	}
+
+}
