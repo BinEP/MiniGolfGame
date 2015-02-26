d621e0f8e3716ab920417f2d7e9b7685fdb0b386
BinEP <bradystoffel@gmail.com>
2015-02-25 19:21:22 -0600

    Texture class

---
+++ src/templates/Texture.java
@@ -0,0 +1,22 @@
+package templates;
+
+public class Texture {
+
+	public double t = MEDIUM;
+	
+	public Texture(int t) {
+		// TODO Auto-generated constructor stub
+		this.t = t;
+	}
+	
+	public static final double HARD = .2;
+	public static final double CLIPPED = .6;
+	public static final double MEDIUM = .9;
+	public static final double GROWN = 2;
+	public static final double SAND = 3;
+	public static final double WATER = 10;
+
+	public double getTexture() {
+		return t;
+	}
+}
