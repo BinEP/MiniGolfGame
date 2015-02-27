bc4e6629b8f85037ef8e51e4c6c56ffd1c4dfe6e
BinEP <bradystoffel@gmail.com>
2015-02-26 23:44:05 -0600

    JPanel rows and auto disable other players

--- src/game/Setup.java
+++ src/game/Setup.java
@@ -1,5 +1,6 @@
 package game;
 
+import java.awt.BorderLayout;
 import java.awt.Color;
 import java.awt.FlowLayout;
 import java.awt.GridLayout;
@@ -9,6 +10,7 @@ import java.awt.event.ActionListener;
 import javax.swing.BorderFactory;
 import javax.swing.Box;
 import javax.swing.JComponent;
+import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JOptionPane;
 import javax.swing.JPanel;
@@ -21,9 +23,9 @@ public class Setup {
 		Setup s = new Setup();
 		JLabel message = new JLabel("Let's Play Mini Golf!", JLabel.CENTER);
 
-		JPanel players = createRow("How many players?", "1", 1);
+		JPanel players = createRow("How many players?", "1", 1, true);
 
-		JPanel p1 = createRow("Name?", "Player", 5);
+		JPanel p1 = createRow("Name?", "Player", 5, true);
 		JPanel p2 = createRow("Name?", 5);
 		JPanel p3 = createRow("Name?", 5);
 		JPanel p4 = createRow("Name?", 5);
@@ -100,7 +102,7 @@ public class Setup {
 
 		inputPanel.add(p4);
 
-		while (true) {
+//		while (true) {
 
 			int action = JOptionPane.showConfirmDialog(null, inputPanel,
 					"Mini Golf", JOptionPane.OK_CANCEL_OPTION,
@@ -108,8 +110,26 @@ public class Setup {
 
 			if (action != JOptionPane.OK_OPTION)
 				return;
+			
+			JFrame frame = new JFrame("Mini Golf!");
 
-		}
+			frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+			frame.setLayout(new BorderLayout());
+			
+			//so doesn't flicker
+			frame.setResizable(false);
+			frame.setAlwaysOnTop(false);
+			
+//			frame.setAlwaysOnTop(true);
+
+			GamePanel game = new GamePanel(Integer.parseInt(((JTextField) players.getComponent(2)).getText()));
+//			frame.add((Component) gameClass.cast(game), BorderLayout.CENTER);
+			frame.add(game, BorderLayout.CENTER);
+
+			frame.setSize(500, 500);
+			frame.setVisible(true);
+
+//		}
 
 	}
 
@@ -123,7 +143,22 @@ public class Setup {
          row.setLayout(new FlowLayout(FlowLayout.LEFT));        
          row.add(Box.createHorizontalStrut(40));
          row.add(new JLabel(labelText));
-         row.add(new JTextField(textWidth));
+         JTextField jt = new JTextField(textWidth);
+         jt.setEnabled(false);
+         row.add(jt);
+         return row;
+    	
+    }
+	
+	public static JPanel createRow(String labelText, int textWidth, boolean enabled) {
+    	JPanel row = new JPanel();
+    	 row = new JPanel();
+         row.setLayout(new FlowLayout(FlowLayout.LEFT));        
+         row.add(Box.createHorizontalStrut(40));
+         row.add(new JLabel(labelText));
+         JTextField jt = new JTextField(textWidth);
+         jt.setEnabled(enabled);
+         row.add(jt);
          return row;
     	
     }
@@ -134,7 +169,22 @@ public class Setup {
          row.setLayout(new FlowLayout(FlowLayout.LEFT));        
          row.add(Box.createHorizontalStrut(40));
          row.add(new JLabel(labelText));
-         row.add(new JTextField(defaultText, textWidth));
+         JTextField jt = new JTextField(defaultText, textWidth);
+         jt.setEnabled(false);
+         row.add(jt);
+         return row;
+    	
+    }
+	
+	public static JPanel createRow(String labelText, String defaultText, int textWidth, boolean enabled) {
+    	JPanel row = new JPanel();
+    	 row = new JPanel();
+         row.setLayout(new FlowLayout(FlowLayout.LEFT));        
+         row.add(Box.createHorizontalStrut(40));
+         row.add(new JLabel(labelText));
+         JTextField jt = new JTextField(defaultText, textWidth);
+         jt.setEnabled(enabled);
+         row.add(jt);
          return row;
     	
     }
