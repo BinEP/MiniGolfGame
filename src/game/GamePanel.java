05f71e201be1d966b756f3549f7e0ddc079ebcfc
BinEP <bradystoffel@gmail.com>
2015-02-27 09:56:57 -0600

    Point stored in doubles to not curve at end

--- src/game/GamePanel.java
+++ src/game/GamePanel.java
@@ -6,6 +6,7 @@ import java.awt.Graphics2D;
 import java.awt.LayoutManager;
 import java.awt.MouseInfo;
 import java.awt.Point;
+import java.awt.Rectangle;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.awt.event.KeyEvent;
@@ -36,12 +37,13 @@ public class GamePanel extends JPanel implements ActionListener, KeyListener,
 
 	public int holeNum = 1;
 
-	public Point ball;
+	public double ballX;
+	public double ballY;
 	public boolean ballMoving = false;
 	public double ballSpeed = 0;
 
-	public int deltaX = 0;
-	public int deltaY = 0;
+	public double deltaX = 0;
+	public double deltaY = 0;
 
 	public GamePanel(int pNum) {
 		// TODO Auto-generated constructor stub
@@ -111,14 +113,15 @@ public class GamePanel extends JPanel implements ActionListener, KeyListener,
 
 	public void setBall() {
 
-		ball = holes.get(holeNum - 1).getStart();
+		ballX = holes.get(holeNum - 1).getStart().getX();
+		ballY = holes.get(holeNum - 1).getStart().getY();
 
 	}
 
 	public void drawBall(Graphics2D g) {
 		// TODO Auto-generated method stub
 		g.setColor(Color.BLACK);
-		g.fillOval(ball.x, ball.y, 10, 10);
+		g.fillOval((int) ballX, (int) ballY, 10, 10);
 	}
 
 	public void drawHole(Graphics2D g) {
@@ -141,21 +144,21 @@ public class GamePanel extends JPanel implements ActionListener, KeyListener,
 	public void moves() {
 		// TODO Auto-generated method stub
 
-		ball.x += deltaX;
-		ball.y += deltaY;
+		ballX += deltaX;
+		ballY += deltaY;
 
-		CShape s = hole.getShape(ball);
-		System.out.println("Texture: " + s.getTexture());
-
+//		CShape s = hole.getShape(ball);
+//		System.out.println("Texture: " + s.getTexture());
+		
 		if (ballMoving) {
-			deltaX *= s.getTexture();
-			deltaY *= s.getTexture();
+			double angle = Math.atan(deltaY / deltaX);
+			ballSpeed *= 0.9;
+			deltaX = ballSpeed * Math.cos(angle) * (deltaX < 0 ? -1 : 1);
+			deltaY = ballSpeed * Math.sin(angle) * (deltaX < 0 ? -1 : 1);
 		}
 		System.out.println("Deltas");
 		System.out.println("dx:\t" + deltaX);
 		System.out.println("dy:\t" + deltaY);
-
-		ballSpeed *= (s.getTexture() * .7 * .7);
 
 		System.out.println("Ball Speed: " + ballSpeed);
 		checkBallFinished();
@@ -169,8 +172,9 @@ public class GamePanel extends JPanel implements ActionListener, KeyListener,
 
 		Point h = holes.get(holeNum - 1).getHole();
 		Ellipse2D.Double eh = new Ellipse2D.Double(h.x + 5, h.y + 5, 10, 10);
+		Rectangle ba = new Rectangle((int) ballX + 2, (int) ballY + 2, 6, 6);
 
-		if (eh.contains(h)) {
+		if (eh.intersects(ba)) {
 			nextHole();
 
 		}
@@ -179,8 +183,10 @@ public class GamePanel extends JPanel implements ActionListener, KeyListener,
 
 	public void nextHole() {
 		// TODO Auto-generated method stub
-		holeNum++;
-		if (!(holeNum > holes.size())) {
+		
+		if (!(holeNum + 1 > holes.size())) {
+			holeNum++;
+			System.out.println("Next hole");
 			hole = holes.get(holeNum - 1);
 			setBall();
 
@@ -228,43 +234,49 @@ public class GamePanel extends JPanel implements ActionListener, KeyListener,
 
 			// Point mouseP = MouseInfo.getPointerInfo().getLocation();
 			Point mouseP = e.getPoint();
+			mouseP.x -= 5;
+			mouseP.y -= 5;
 			// Point screenP = this.getLocationOnScreen();
 			//
 			// Point relativeP = new Point(mouseP.x - screenP.x, mouseP.y -
 			// screenP.y);
-			Point relativeP = new Point(mouseP.x - ball.x, mouseP.y - ball.y);
+			Point relativeP = new Point(mouseP.x - (int) ballX, mouseP.y - (int) ballY);
 
 			// relativeP.x *= 3;
 			// relativeP.y *= 3;
 
-			double d = relativeP.distance(ball);
-			System.out.println("Distance: " + d);
-			ballSpeed = d;
+//			double d = relativeP.distance(ball);
+//			double d = mouseP.distance(ball);
+//			System.out.println("Distance: " + d);
+//			ballSpeed = d;
 
-			int numerator = relativeP.y;
-			numerator -= numerator % 5;
+//			int numerator = relativeP.y;
+//			numerator -= numerator % 5;
 
-			int denominator = relativeP.x;
-			denominator -= denominator % 5;
+//			int denominator = relativeP.x;
+//			denominator -= denominator % 5;
 
 			// System.out.println("Rounded to 5");
 			// System.out.println(numerator);
 			// System.out.println(denominator);
 
-			int div = Math.abs(gcm(numerator, denominator));
-			numerator /= div;
-			denominator /= div;
+//			int div = Math.abs(gcm(numerator, denominator));
+//			numerator /= div;
+//			denominator /= div;
 
 			// deltaY = (int) Math.ceil((double) numerator / 10);
 			// deltaX = (int) Math.ceil((double) denominator / 10);
 
-			System.out.println("Lowest common denom");
-			System.out.println(numerator);
-			System.out.println(denominator);
+//			System.out.println("Lowest common denom");
+//			System.out.println(numerator);
+//			System.out.println(denominator);
 
-			deltaY = (int) (numerator * 1.7);
-			deltaX = (int) (denominator * 1.7);
+//			deltaY = (int) (numerator * 1.7);
+//			deltaX = (int) (denominator * 1.7);
+			deltaY = relativeP.y / 9d;
+			deltaX = relativeP.x / 9d;
 			ballMoving = true;
+			ballSpeed = Math.sqrt((deltaX * deltaX) + (deltaY * deltaY));
 
 		}
 
